05186f65dfe80c4187d02c7a4ead910122dbf60e
Joao Batista <jpcbatista.2004@gmail.com>
2023-11-27 12:37:18 -0300

    🏃 Adding shell file to test the models

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+python3 ./code/server.py --world_size 4 --dataset_id 1 --lr 0.001 &
+
+python3 ./code/client.py --world_size 4 --rank 1 --epoch 100 --dataset_id 1 --batch_size 32 --lr 0.001 &
+python3 ./code/client.py --world_size 4 --rank 2 --epoch 200 --dataset_id 2 --batch_size 32 --lr 0.0001 &
+python3 ./code/client.py --world_size 4 --rank 3 --epoch 300 --dataset_id 3 --batch_size 32 --lr 0.001 &
